6104a1c877f2ab4a74c22d50f0b6914dce45e3b0
canine89 <phk707kr@gmail.com>
2021-10-15 11:38:19 +0900

    isbn search added(not perfect).

--- frontend/src/components/BookStrategy.js
+++ frontend/src/components/BookStrategy.js
@@ -21,6 +21,10 @@ const BookStrategy = () => {
   const [title, setTitle] = useState('');
   const [gridSize, setGridSize] = useState('grid grid-cols-1 gap-4');
 
+  const [book1, setBook1] = useState(-1);
+  const [isbnData1, setIsbnData1] = useState([]);
+  const [title1, setTitle1] = useState('');
+
   const columns = [
     {
       name: '순위',
@@ -174,6 +178,24 @@ const BookStrategy = () => {
     setGridSize(gridSize);
   };
 
+  const book1OnChangeHandler = (e) => {
+    console.log(e.target.value);
+    setBook1(e.target.value);
+  };
+
+  const onIsbnClickHander = (e) => {
+    console.log('http://175.211.105.9:8000/book/isbn/?id=' + book1);
+
+    fetch('http://175.211.105.9:8000/book/isbn/?id=' + book1)
+      .then((response) => {
+        return response.json();
+      })
+      .then((_json) => {
+        setIsbnData1(_json);
+        setTitle1(_json[0].book.title);
+      });
+  };
+
   return (
     <>
       <div className="flex flex-row space-x-4 items-center pl-5 py-4 bg-red-400">
@@ -256,6 +278,41 @@ const BookStrategy = () => {
           <div>
             {isbnData.length > 0 ? (
               <GraphWithIsbn isbnData={isbnData} title={title} />
+            ) : (
+              <Loading />
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-row space-x-4 items-center pl-5 py-2 bg-green-400">
+          <div className="text-white font-extrabold">
+            <span>ISBN 검색 그래프</span>
+          </div>
+        </div>
+        <div className="flex flex-row space-x-4 items-center pl-5 py-2 bg-green-100">
+          <div className="font-extrabold">
+            <span>ISBN</span>
+            <span className="pl-4">
+              <input
+                className="rounded-md border text-sm text-gray-600 pl-2 py-1 mr-4 font-semibold border-gray-200"
+                name="book1"
+                onChange={book1OnChangeHandler}
+              />
+              <button
+                className="rounded-md border px-2 py-1 font-semibold text-white bg-green-400"
+                onClick={onIsbnClickHander}
+              >
+                검색!
+              </button>
+            </span>
+          </div>
+        </div>
+        <div className={gridSize + ' p-2'}>
+          <div>
+            {isbnData1.length > 0 ? (
+              <GraphWithIsbnOptions isbnData={isbnData1} title={title1} />
             ) : (
               <Loading />
             )}
