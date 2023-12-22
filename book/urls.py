5f373482c70942bee32157588f57854fc8a91a2d
canine89 <phk707kr@gmail.com>
2023-12-22 18:33:42 +0900

    book/views 정리 완료.

--- book/urls.py
+++ book/urls.py
@@ -4,34 +4,18 @@ from . import views
 urlpatterns = [
     re_path(
         "^$",
-        view=views.Book.as_view(),
+        view=views.FakeIsbnBooks.as_view(),
     ),
     re_path(
-        "^date/$",
-        view=views.DatetimeRangeBook.as_view(),
+        "^daterange/$",
+        view=views.DateRangeBooks.as_view(),
     ),
     re_path(
         "^datelist/$",
-        view=views.DateListBook.as_view(),
-    ),
-    re_path(
-        "^fakeisbn/$",
-        view=views.FakeIsbnBook.as_view(),
-    ),
-    re_path(
-        "^publisher/$",
-        view=views.PublisherBook.as_view(),
+        view=views.DateList.as_view(),
     ),
     re_path(
         "^yes24/status/$",
         view=views.Yes24Status.as_view(),
-    ),
-    re_path(
-        "^publisher/easyspub/$",
-        view=views.EasysBooks.as_view(),
-    ),
-    re_path(
-        "^isbn/$",
-        view=views.FullInfoBook.as_view(),
     ),
 ]
--- book/views.py
+++ book/views.py
@@ -10,41 +10,7 @@ from itertools import chain
 from collections import Counter
 
 
-class Book(APIView):
-    def get(self, format=None):
-        _datetime = datetime.now()
-        metadatas = models.Metadata.objects.filter(
-            crawl_date__year=_datetime.year,
-            crawl_date__month=_datetime.month,
-            crawl_date__day=_datetime.day,
-        ).order_by("rank")
-
-        serializer = serializers.MetadataSerializer(
-            metadatas,
-            many=True,
-        )
-
-        return Response(data=serializer.data)
-
-
-class FullInfoBook(APIView):
-    def get(self, format=None):
-        _datetime = datetime.now()
-        metadatas = models.Metadata.objects.filter(
-            crawl_date__year=_datetime.year,
-            crawl_date__month=_datetime.month,
-            crawl_date__day=_datetime.day,
-        ).order_by("rank")
-
-        serializer = serializers.MetadataSerializer(
-            metadatas,
-            many=True,
-        )
-
-        return Response(data=serializer.data)
-
-
-class DateListBook(APIView):
+class DateList(APIView):
     folderName = "./yes24/linting_dump_datas"
 
     def get(self, request, format=None):
@@ -57,7 +23,7 @@ class DateListBook(APIView):
         return Response(sorted(datelist, reverse=True))
 
 
-class DatetimeRangeBook(APIView):
+class DateRangeBooks(APIView):
     def get(self, request, format=None):
         year = request.query_params.get("year", None)
         month = request.query_params.get("month", None)
@@ -77,13 +43,15 @@ class DatetimeRangeBook(APIView):
         return Response(data=serializer.data)
 
 
-class FakeIsbnBook(APIView):
+class FakeIsbnBooks(APIView):
     def get(self, request, format=None):
-        isbn = request.query_params.get("id", None)
+        fisbn = request.query_params.get("fisbn", None)
 
-        metadatas = models.Metadata.objects.filter(Q(book__fake_isbn=isbn)).order_by(
+        metadatas = models.Metadata.objects.filter(Q(book__fake_isbn=fisbn)).order_by(
             "crawl_date"
         )
+
+        print(metadatas)
 
         serializer = serializers.MetadataSerializer(
             metadatas,
@@ -91,43 +59,6 @@ class FakeIsbnBook(APIView):
         )
 
         return Response(data=serializer.data)
-
-
-class PublisherBook(APIView):
-    def get(self, request, format=None):
-        publisher = request.query_params.get("publisher", None)
-
-        metadatas = models.Metadata.objects.select_related("book").filter(
-            market="yes24",
-            book__publisher=publisher,
-        )
-
-        serializer = serializers.MetadataSerializer(metadatas, many=True)
-
-        return Response(data=serializer.data)
-
-
-class PublisherStatus(APIView):
-    def get(self, request, format=None):
-        year = request.query_params.get("year", None)
-        month = request.query_params.get("month", None)
-        day = request.query_params.get("day", None)
-
-        result = (
-            models.Metadata.objects.filter(
-                crawl_date__year=year, crawl_date__month=month, crawl_date__day=day
-            )
-            .values("book__publisher")
-            .annotate(
-                sales_point_sum=Sum("sales_point"),
-                sales_point_avg=Avg("sales_point"),
-                rank_avg=Avg("rank"),
-                number_of_book=Count("book__title"),
-            )
-            .order_by("-sales_point_sum")
-        )
-
-        return Response(result)
 
 
 class Yes24Status(APIView):
@@ -151,11 +82,3 @@ class Yes24Status(APIView):
         )
 
         return Response(result)
-
-
-class EasysBooks(APIView):
-    def get(self, request, format=None):
-        books = models.Book.objects.filter(publisher="이지스퍼블리싱")
-        serializer = serializers.BookSerializer(books, many=True)
-
-        return Response(data=serializer.data)
